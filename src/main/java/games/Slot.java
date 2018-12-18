117782c20546ecda0f8f226d48b58628dde268eb
Chimit Menzhikov <chimit@mosk.ru>
2018-12-18 12:07:27 +0700

    add Slot game release 1.0

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -1,8 +1,53 @@
 package games;
 
+import static java.lang.Math.random;
+import static java.lang.Math.round;
+
 public class Slot {
 
-    public static void main(String... __) {
-        System.out.println("Hello, World!");
+  private static final int SIZE = 7;
+  private static final int BET = 10;
+  private static final int PRIZE = 1_000;
+
+  private static int cash = 100;
+  private static String[] msg = {"У Вас %d$, ставка - %d$",
+      "Крутим барабаны! Розыгрыш принёс следующие результаты:",
+      "первый барабан - %d, второй - %d, третий - %d",
+      "Проигрыш %d$, ваш капитал теперь составляет: %d$",
+      "Выигрыш %d$, ваш капитал теперь составляет: %d$"};
+  private static int firstCounter;
+  private static int secondCounter;
+  private static int thirdCounter;
+
+  /**
+   * Entry point.
+   * @param args from command line.
+   */
+  public static void main(String... args) {
+
+    while (cash > 0) {
+      display(String.format(msg[0], cash, BET));
+      display(msg[1]);
+      firstCounter = roll(firstCounter);
+      secondCounter = roll(secondCounter);
+      thirdCounter = roll(thirdCounter);
+      display(String.format(msg[2], firstCounter, secondCounter, thirdCounter));
+      if (firstCounter == secondCounter && secondCounter == thirdCounter) {
+        cash += PRIZE;
+        display(String.format(msg[3], PRIZE, cash));
+      } else {
+        cash -= BET;
+        display(String.format(msg[3], BET, cash));
+      }
     }
+
+  }
+
+  private static void display(String msg) {
+    System.out.println(msg);
+  }
+
+  private static int roll(int counter) {
+    return (counter + (int) round(random() * 100)) % SIZE;
+  }
 }
